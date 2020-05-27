de25ea75c873a1b6563fa83a8a186f9de01f201d
AJB <jungbok0307@gmail.com>
2020-05-28 04:03:48 +0900

    edit code: add name of each times table and set return 0

--- mul_table.c
+++ mul_table.c
@@ -2,10 +2,14 @@
 
 int main()
 {
+	printf("구구단을 출력합니다.\n\n");
 	for (int i=1; i<10; i++){
+		printf("%d 단\n\n", i);
 		for (int j=1; j<10; j++) 
 		{
 			printf("%d * %d = %d\n", i, j, i*j);
 		}
+		printf("\n");
 	}
+	return 0;
 }
