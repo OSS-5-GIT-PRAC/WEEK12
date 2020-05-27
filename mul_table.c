8c7c521920b4ed7b0db578607e11a97d2bae3a7b
jb0307 <61444802+jb0307@users.noreply.github.com>
2020-05-28 02:12:26 +0900

    Update mul_table.c
    edit code: add name of each times table and set return 0

--- mul_table.c
+++ mul_table.c
@@ -3,9 +3,12 @@
 int main()
 {
 	for (int i=1; i<10; i++){
+		printf("%d 단\n\n", i);
 		for (int j=1; j<10; j++) 
 		{
 			printf("%d * %d = %d\n", i, j, i*j);
 		}
+		printf("\n");
 	}
+	return 0;
 }
