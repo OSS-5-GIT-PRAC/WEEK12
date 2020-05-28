7539635408dcb659b0942c0d93a8cc343bba7c69
heesungkim010 <heesungkim010@gmail.com>
2020-05-28 10:28:52 +0900

    edit codes

--- mul_table.c
+++ mul_table.c
@@ -2,10 +2,11 @@
 
 int main()
 {
+        int i, j;
 	printf("구구단을 출력합니다.\n\n");
-	for (int i=1; i<10; i++){
+	for (i=1; i<10; i++){
 		printf("%d 단\n\n", i);
-		for (int j=1; j<10; j++) 
+		for (j=1; j<10; j++) 
 		{
 			printf("%d * %d = %d\n", i, j, i*j);
 		}
